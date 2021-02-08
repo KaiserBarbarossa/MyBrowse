5fcf1f405cf4f21bd63de1df15edd9482523603f
KaiserBarbarossa <kaiser.barbarossa@yandex.com>
2021-02-08 20:44:14 +0100

    Quote command lines args

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ $1 == "install" ]
+if [ "$1" == "install" ]
 	then
 		cd /tmp
 		git clone https://github.com/KaiserBarbarossa/MyBrowse.git
 		sudo cp /tmp/MyBrowse/mybrowse.py /usr/local/bin/
 		mkdir ~/.config/mybrowse
 		cp /tmp/MyBrowse/mybrowse.cfg ~/.config/mybrowse/mybrowse.cfg
-	elif [ $1 == "remove" ]
+	elif [ "$1" == "remove" ]
 		then
 			echo "MyBrowse wird entfernt..."
 			sudo rm  /usrl/local/bin/mybrowse.py		
@@ -21,7 +21,7 @@ if [ $1 == "install" ]
 					*) echo "Unbekanntes Parameter - Konfigurationsdateien werden nicht entfernt"
 						;;
 			esac
-	elif [ $1 == "upgrade" ]
+	elif [ "$1" == "upgrade" ]
 		then
 			sudo rm /usr/local/bin/mybrowse.py
 			git clone https://github.com/KaiserBarbarossa/MyBrowse.git /tmp/MyBrowse
