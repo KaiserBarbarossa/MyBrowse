8875aa9c5b17930859b319f6b2b9cb11f5b54a24
KaiserBarbarossa <kaiser.barbarossa@yandex.com>
2025-08-06 15:56:03 +0200

    Bump copyright and fix comment format

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
-#Copyright (C) 2021-2023  tuxifreund <kaiser.barbarossa@yandex.com>
+# Copyright (C) 2021-2025  tuxifreund <kaiser.barbarossa@yandex.com>
 #
-#This file is part of MyBrowse.
+# This file is part of MyBrowse.
 #
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import setuptools
 
